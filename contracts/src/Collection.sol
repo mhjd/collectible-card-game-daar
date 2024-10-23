34c8a19d5715441d705136aa00bcef710eb08fdc
Mohammad-Habib.Javaid <mohammad-habib.javaid@etu.sorbonne-universite.fr>
2024-10-23 17:11:28 +0200

    ajout des fichers yaern, package, et màj du readme (jsp pq)

--- contracts/src/Collection.sol
+++ contracts/src/Collection.sol
@@ -26,13 +26,23 @@ contract Collection is ERC721 {
       modelCardCount++;
       return id;
   }
-  function _createCard(int cardNumber) returns(int) {
+  function getNewId() {
+      
+  }
+  function _createCard() returns(int) {
     /* return myCards.push(Card(pathImg, cardNumber)) - 1; */
     uint id = myCards.push(cardNumber) - 1;
     
     return id;
     // zombieToOwner[id] = msg.sender; // faudra l'implémenter ça, vu que chaque carte est unique
     // ownerZombieCount[msg.sender] = ownerZombieCount[msg.sender].add(1);
+  }
+
+  // retourne une carte au hasard dans la collection
+  // généré une erreur si il n'y a plus assez de cartes
+  // utile pour ouvrir un deck
+  function getAvailableCard() returns(int) {
+      
   }
 
   constructor(string memory _name, int _cardCount) {
--- contracts/src/Main.sol
+++ contracts/src/Main.sol
@@ -16,11 +16,26 @@ contract Main {
   }
 
   function addCardToCollection(int CollectionId, string pathImg, int cardNumber, int numberOfThisCard) {
+      int id = collection[CollectionId]._createModelCard(pathImg, numberOfCards);
+      for (uint i = 0; i < numberOfThisCard; i++) {
+	  collection[CollectionId]._createCard(id);
+      }
       // for i in range numberofthiscard
       //    collections[CollectionId]._createCard(pathImg, cardNumber)
   }
 
+  // est-ce que faudrait pas plutôt utiliser msg.sender ? plutôt que userId ?
+  // du coup va falloir changer certain mapping, avec adress à la place de int
   function assignCardToUser(int userId, int cardNFTId) {
       CardIdToUser[cardNFTId] = userId; // pseudo code
   }
+
+  // ouverture de deck
+  function assignXCardsToUser(int userId, uint X) {
+      for (uint i = 0; i < X; i++) {
+	  assignCardsToUser(userId, getAvailableCard());
+      }
+  }
+  
+
 }
