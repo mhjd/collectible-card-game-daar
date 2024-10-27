7fa6c8de0fd64add67fdc15865f6470f76c2c207
Abdou78130 <abdallah2901@gmail.com>
2024-10-27 02:10:24 +0200

    Beautiful front update

--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -1,28 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './styles.module.css';
+import logo from './pictures/TCG_logo.png';
 
 export const HomePage: React.FC = () => {
   return (
     <div className={styles.body}>
       <div className={styles.container}>
-        <h1>Welcome to Pokemon TCG Collection</h1>
+        <img src={logo} alt="Pokémon TCG Logo" width="60%" />
         <nav className={styles.navigation}>
           <ul className={styles.noListStyle}>
             <li>
-              <Link to="/sets" className={styles.navLink}>Browse All Sets</Link>
+              <Link to="/sets">
+                <button className={styles.navLink} role="button" ><span className={styles.text}>Extensions</span></button>
+              </Link>
             </li>
             <li>
-              <Link to="/collection" className={styles.navLink}>My Collection</Link>
+              <Link to="/collection">
+                <button className={styles.navLink} role="button"><span className={styles.text}>Ma Collection</span></button>
+              </Link>
             </li>
             <li>
-              <Link to="/open-booster" className={styles.navLink}>Open Booster</Link>
+              <Link to="/open-booster">
+                <button className={styles.navLink} role="button" ><span className={styles.text}>Boosters</span></button>
+              </Link>
             </li>
             <li>
-              <Link to="/chacal" className={styles.navLink}>Test Connection with Solidity</Link>
+              <Link to="/chacal">
+                <button className={styles.navLink} role="button" ><span className={styles.text}>Tests</span></button>
+              </Link>
             </li>
           </ul>
         </nav>
+        {/*<input type="button" className="button-64" role="button"><span className="text">Extensions</span></input>*/}
       </div>
     </div>
   );
