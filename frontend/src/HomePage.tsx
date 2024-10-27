ce9b5e189a36c7794f6b5c4e53266d60a87864be
Abdou78130 <abdallah2901@gmail.com>
2024-10-27 22:34:04 +0100

    up

--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -33,7 +33,7 @@ export const HomePage: React.FC = () => {
             </li>
             <li>
               <Link to="/marketplace">
-                <button className={styles.navLink} role="button"><span className={styles.text}>Marché</span></button>
+                <button className={styles.navLink} role="button"><span className={styles.text}>Marketplace</span></button>
               </Link>
             </li>
             <li>
